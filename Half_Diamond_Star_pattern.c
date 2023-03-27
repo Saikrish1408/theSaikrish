6ccbe35eeceaa8124952f7f393a644e283c37364
Sai Ram Krishna <105543330+Saikrish1408@users.noreply.github.com>
2023-03-27 23:04:45 +0530

    Hald Diamond Star Pattern

---
+++ Half_Diamond_Star_pattern.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int n,k;
+    printf("Enter the Number of Rows: ");
+    scanf("%d",&n);
+    
+    for(int i = 1 ; i <= n ; i++)
+    {
+        for(int j = 1 ; j <= i ; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    for(int i = n-1 ; i >= 1 ; i--)
+    {
+        for(int j = i ; j >= 1 ; j--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
